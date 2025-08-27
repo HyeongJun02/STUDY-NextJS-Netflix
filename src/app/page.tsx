96001143543db49ada34c7b6e8be1b1195e88fb0
HyeongJun, Kim <khj029956@naver.com>
2025-08-27 10:11:47 +0900

    fix: 몰아보기 탭 때문에 발생하던 오류 -> 잠시 주석 처리

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,8 +8,8 @@ export default function Home() {
       <Hero item={HERO} />
       <div className="mt-6 space-y-8">
         <Row title="오늘 대한민국의 TOP 10 시리즈" items={ROW_TOP10} ranked />
-        <Row title="시청 중인 콘텐츠" items={ROW_CONTINUE} />
-        <Row title="몰아보기 추천 시리즈" items={[...ROW_CONTINUE, ...ROW_CONTINUE]} />
+        <Row title="시청 중인 콘텐츠" items={ROW_CONTINUE} wide />
+        {/* <Row title="몰아보기 추천 시리즈" items={[...ROW_CONTINUE, ...ROW_CONTINUE]} /> */}
       </div>
     </main>
   );
